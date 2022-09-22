9589fadde87910de8fe62cacfb6f58e79a6f1eb3
DanielDuchnicki <daniel.duchnicki@gmail.com>
2022-09-22 22:29:30 +0200

    Added Rating

---
+++ EFMigrationsMergeConflictApi/EFMigrationsMergeConflict.Infrastructure/Migrations/20220922202552_AddRating.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFMigrationsMergeConflictApi.Infrastructure.Migrations
+{
+    public partial class AddRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Blogs");
+        }
+    }
+}
